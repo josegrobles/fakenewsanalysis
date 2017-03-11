fd60cdf099a5e8ffb65381df71ac4304b05fb06a
Jose Antonio González Robles <josegrobles1997@gmail.com>
2017-03-11 12:25:37 +0100

    more...

--- routes/index.js
+++ routes/index.js
@@ -80,40 +80,32 @@ router.post('/related', function(req, res, next) {
         if (err) res.end(JSON.stringify({status:"error"}))
         else if (resp === null) res.end(JSON.stringify({status:"send_again"}))
         else {
-          router.post('/related', function(req, res, next) {
-      let {
-          url
-      } = req.body
-      let urinfo = URL.parse(url)
-      client.hget(urinfo.hostname, urinfo.pathname, function(err, resp) {
-          if (err) res.end(JSON.stringify({status:"error"}))
-          else if (resp === null) res.end(JSON.stringify({status:"send_again"}))
-          else {
-              let info = JSON.parse(resp)
-              var options = {
-                  url: "https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=" + info.title + "&count=10&offset=0&mkt=es-es&safeSearch=Moderate",
-                  headers: {
-                      'Ocp-Apim-Subscription-Key': process.env.OCP1
-                  }
-              };
+            let info = JSON.parse(resp)
+            var options = {
+                url: "https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=" + info.title + "&count=10&offset=0&mkt=es-es&safeSearch=Moderate",
+                headers: {
+                    'Ocp-Apim-Subscription-Key': process.env.OCP1
+                }
+            };
 
-              request(options, function(error, response, body) {
-                  if (!error && response.statusCode == 200) {
-                      var info = JSON.parse(body);
-                      if (resp.publishedTime === "") res.end(JSON.stringify(info.value))
-                      else {
-                          let values = info.value
-                          let final = []
-                          for (var i = 0; i < values.length; i++) {
-                              if (moment(resp.publishedTime).isSame(values[i].datePublished, 'day')) final.push(values[i])
-                          }
-                          res.end(JSON.stringify(final))
-                      }
-                  }
-              })
-          }
-      })
-  })
+            request(options, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var info = JSON.parse(body);
+                    if (resp.publishedTime === "") res.end(JSON.stringify(info.value))
+                    else {
+                        let values = info.value
+                        let final = []
+                        for (var i = 0; i < values.length; i++) {
+                            if (moment(resp.publishedTime).isSame(values[i].datePublished, 'day')) final.push(values[i])
+                        }
+                        res.end(JSON.stringify(final))
+                    }
+                }
+            })
+        }
+    })
+})
+
 
 router.post('/languageAnalysis',function(req,res,next){
   let {
